d786a880cb5a0f0505c22b42798324dffe7bd797
wraith0x2b <wraith@archlinux.us>
2009-04-29 15:30:55 +0300

    status

--- conf.h
+++ conf.h
@@ -2,7 +2,7 @@
 #define CONF_H
 
 #define VERSION 	"0.2"
-#define DISPLAY		":1"
+#define DISPLAY		":0"
 #define CURSOR		XC_left_ptr /* default cursor,overrides any custom cursors you may have defined outside the WM. */
 #define CMD_CURSOR	XC_rtl_logo /* cursor to let you know you are in command mode. This is best to differ from CURSOS */
 /* valid cursor names in "/usr/include/X11/cursorfont.h", or anywhere you can find them on the internet. Defaults should be fine though */
